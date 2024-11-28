676d70222ef4d8dfb6d7bf996dddd88111f465f7
Cristhian David Recalde <crecalde@neaternotes.com>
2024-11-27 23:06:46 -0500

    addnodejs stand alone

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,15 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from "@astrojs/preact";
 
+import node from '@astrojs/node';
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  }),
   srcDir: "./src",
 });
