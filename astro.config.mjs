9cd236f5aac87f6383dc3b3d37294976207ef699
Cristhian David Recalde <crecalde@neaternotes.com>
2024-11-28 22:43:12 -0500

    new config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   srcDir: "./src",
 });
